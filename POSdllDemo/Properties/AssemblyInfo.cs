c84cce7627323c78e258c4d2eb22864bbc0b06f2
ghfeng <ghfeng@im-vr.com>
2018-08-14 21:00:53 +0800

    Signed-off-by: ghfeng <ghfeng@im-vr.com>

---
+++ POSdllDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GprinterDEMO")]
+[assembly:AssemblyDescription("")]
+[assembly:AssemblyConfiguration("")]
+[assembly:AssemblyCompany("")]
+[assembly: AssemblyProduct("GprinterDEMO")]
+[assembly:AssemblyCopyright("Copyright china 2014")]
+[assembly:AssemblyTrademark("")]
+[assembly:AssemblyCulture("")]
+
+
+[assembly:ComVisible(false)]
+
+[assembly:Guid("e445b98b-5b51-4b29-994f-f7abe3594a69")]
+
+[assembly:AssemblyVersion("1.0.0.0")]
+[assembly:AssemblyFileVersion("1.0.0.0")]
